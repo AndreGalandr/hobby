09703b6c7daca7dee5f9031347521fbc50c03c50
Andre Mirny <sevenlogiks@gmail.com>
2017-01-16 22:04:23 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+ // window.onload = function() {
+ //        var el1 = document.getElementsByClassName("cbalink")[0];
+ //        var el2 = document.getElementsByClassName("cumf_bt_form_wrapper")[0];
+ //        if (!!el1.parentNode) el1.parentNode.removeChild(el1);
+ //        if (!!el2.parentNode) el2.parentNode.removeChild(el2);
+ //      };
